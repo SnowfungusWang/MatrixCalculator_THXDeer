b7e1767953bd46b30f7eecd7f9d2481145f24d66
lfm <1071956678@qq.com>
2019-03-14 21:40:12 +0800

    update

---
+++ client/my-blog/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+  html,
+  body,
+  #app {
+    margin: 0;
+    height: 100%;
+    width: 100%;
+    color: #3D3D3D;
+    position: relative;
+  }
+</style>
---
+++ client/my-blog/src/views/login/index.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="login-main">
+    <div class="site-body">
+      <div class="login-body">
+        <el-row>
+          <el-col span='int(15)'>
+            <span>小宝贝的姓名</span>
+          </el-col>
+          <el-col span=14>
+            <el-input></el-input>
+          </el-col>
+        </el-row>
+        <el-row>
+          <el-col span=10>
+            <span>小宝贝的Token</span>
+          </el-col>
+          <el-col span=14>
+            <el-input></el-input>
+          </el-col>
+        </el-row>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Login",
+    created: function () {
+      console.log('hello~~');
+    }
+
+  }
+</script>
+
+<style lang="scss">
+  .login-main {
+    background-color: #dedbdb;
+    height: calc(100%);
+    display: flex;
+    justify-content: center;
+    .site-body {
+      margin-top: 9%;
+      background-color: #efefef;
+      width: 45%;
+      height: 50%;
+      box-shadow: 10px 10px 5px #b4b4b4;
+      display: flex;
+      .login-body {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        .el-row {
+          display: flex;
+          margin-bottom: 2%;
+          span {
+            white-space: nowrap;
+            overflow: hidden;
+            display: flex;
+            align-items: center;
+            justify-content: flex-end;
+            padding-right: 8%;
+          }
+        }
+      }
+    }
+  }
+</style>
