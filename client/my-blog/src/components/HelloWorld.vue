cda1893cac902b0bc6d6bb19fd176ab08246540a
lfm <1071956678@qq.com>
2019-03-19 09:57:33 +0800

    3

--- client/my-blog/src/components/HelloWorld.vue
+++ client/my-blog/src/components/HelloWorld.vue
@@ -3,18 +3,19 @@
 </template>
 
 <script>
-  export default {
-    name: 'HelloWorld',
-    data() {
-      return {
-        msg: 'Welcome to Your Vue.js App'
-      }
-    }
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      msg: "Welcome to Your Vue.js App"
+    };
   }
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss">
-div{
+div {
+  
 }
 </style>
